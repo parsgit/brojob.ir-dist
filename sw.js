9d7711e6f43f192ad5caac32f9aa8a73db3e0279
parsgit <nvioir@gmail.com>
2024-06-01 12:18:39 +0000

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('/workbox/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
